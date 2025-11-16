d0dc8aa48a5e0ba8d1bc4e370f3a9f69f0cfbb5d
arhamshafi <arhambutt2923@gmail.com>
2025-11-16 21:23:41 +0500

    dynamic routing is donw

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -1,10 +1,14 @@
+"use client";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 
 export default function adminlayout({ children }) {
+    const router = useRouter()
     return (
         <>
             <nav className="w-full h-[50px] flex justify-between items-center px-5 py-10  ">
-                <div className="font-bold text-2xl cursor-context-menu flex items-center gap-5 "> <Link href={"/user"} className="text-black font-normal text-sm" >Back</Link>  Welcome Arham Shafi 👋</div>
+                <div className="font-bold text-2xl cursor-context-menu flex items-center gap-5 "> <p onClick={() => router.back()} className="text-black font-normal text-sm" >Back</p>  Welcome Arham Shafi 👋</div>
                 <ul className="w-max flex justify-center items-center gap-5 text-black text-md">
                     {[
                         { title: "Admin Dashboard", link: "/admin" },
